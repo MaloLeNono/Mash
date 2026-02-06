8cec3c08680ae4ff9cc7f46adffe21ae685b640c
Gabriel Malo <malo.gabriel.2@gmail.com>
2026-02-05 20:47:26 -0500

    Removed Old Project

--- Mash/Application/Commands/CommandContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mash.Application.Commands;
-
-public class CommandContext(string workingDirectory, string userRootDirectory, List<ICommand> commands)
-{
-    public string WorkingDirectory { get; set; } = workingDirectory;
-    public string UserRootDirectory { get; } = userRootDirectory;
-    public List<ICommand> RegisteredCommands { get; } = commands;
-}
--- Mash/Application/Commands/ICommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mash.Application.Commands;
-
-public interface ICommand
-{
-    public string Name { get; }
-    public int MinParameterCount { get; }
-    public void Execute(string[] input);
-    public void PrintHelp();
-}
